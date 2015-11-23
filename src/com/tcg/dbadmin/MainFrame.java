54e885dd20b1d63c51cc02c1fce7f3cdde975e31
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2015-11-23 15:14:22 -0800

    Added Update And Delete to Edit Pane

--- src/com/tcg/dbadmin/MainFrame.java
+++ src/com/tcg/dbadmin/MainFrame.java
@@ -36,9 +36,33 @@ public class MainFrame extends JFrame {
 	ArrayList<JTextArea> editAreas;
 	JButton editPrev;
 	JButton editNext;
+	JButton editUpdate;
+	JButton editDeleteRow;
 	
 	public MainFrame(JFrame caller) {
 		connect();
+		
+		initTop();
+		
+		tabbedView = new JTabbedPane();
+		
+		initTablePane();
+		initEditPane();
+		
+		tabbedView.addTab("Browse", browseScrollPane);
+		tabbedView.addTab("Edit", editPanel);
+
+		getContentPane().add(top, BorderLayout.NORTH);
+		getContentPane().add(tabbedView, BorderLayout.CENTER);
+		
+		setSize(800, 600);
+		setLocationRelativeTo(caller);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		updateTitle();
+		setVisible(true);
+	}
+	
+	private void initTop() {
 		
 		top = new JPanel();
 		top.setLayout(new FlowLayout());
@@ -53,19 +77,24 @@ public class MainFrame extends JFrame {
 			ProgramManager.currentTableIndex = tableComboBox.getSelectedIndex();
 			updateTitle();
 			setTableToDefault();
+			editContentPanel.removeAll();
 			initEdit();
 		});
 		
 		top.add(changeTableLabel);
 		top.add(tableComboBox);
 		top.add(changeTableButton);
-		
-		tabbedView = new JTabbedPane();
+	}
+
+	private void initTablePane() {
 		
 		browseScrollPane = new JScrollPane();
 		browseTable = new JTable();
 		browseScrollPane.setViewportView(browseTable);
 		setTableToDefault();
+	}
+	
+	private void initEditPane() {
 
 		editContentPanel = new JPanel();
 		editLabels = new ArrayList<>();
@@ -73,11 +102,16 @@ public class MainFrame extends JFrame {
 
 		editPrev = new JButton("<");
 		editNext = new JButton(">");
+
+		editUpdate = new JButton("Submit Update");
+		editDeleteRow = new JButton("Delete This Row");
 		
 		editButtons = new JPanel();
 		editButtons.setLayout(new FlowLayout());
 		editButtons.add(editPrev);
 		editButtons.add(editNext);
+		editButtons.add(editUpdate);
+		editButtons.add(editDeleteRow);
 		
 		editPrev.addActionListener(e -> {
 			try {
@@ -105,25 +139,103 @@ public class MainFrame extends JFrame {
 			}
 		});
 		
+		editUpdate.addActionListener(e -> {
+			DatabaseMetaData meta = null;
+			try {
+				meta = sqlCon.getMetaData();
+				ResultSet set = meta.getPrimaryKeys(null, null, ProgramManager.getTable());
+				String primary = "", key = "";
+				while(set.next()) {
+				      primary = set.getString("COLUMN_NAME");
+				      System.out.println("getPrimaryKeys(): columnName=" + primary);
+				}
+				key = editResultSet.getString(primary);
+				String sql = String.format("UPDATE %s SET", ProgramManager.getTable());
+				ArrayList<String> colNames = new ArrayList<>();
+				ArrayList<String> colValues = new ArrayList<>();
+				for(JLabel jl : editLabels) {
+					colNames.add(jl.getText());
+				}
+				for(JTextArea jta : editAreas) {
+					colValues.add(jta.getText());
+				}
+				for(int i = 0; i < colNames.size(); i++) {
+					String col = colNames.get(i);
+					String value = String.format("'%s'", colValues.get(i));
+					if(i + 1 == colNames.size()) {
+						sql += String.format(" %s = %s", col, value);
+					} else {
+						sql += String.format(" %s = %s,", col, value);
+					}
+				}
+				sql += String.format(" WHERE %s = '%s';", primary, key);
+				System.out.println(sql);
+				boolean retry = false;
+				do {
+					try {
+						sqlState.executeUpdate(sql);
+						JOptionPane.showMessageDialog(this, "Updated Row", "Success", JOptionPane.INFORMATION_MESSAGE);
+					} catch (Exception e2) {
+						if(e2 instanceof CommunicationsException) {
+							connect();
+							retry = true;
+						} else {
+							JOptionPane.showMessageDialog(this, e2.getMessage(), e2.getClass().getName(), JOptionPane.ERROR_MESSAGE);
+							e2.printStackTrace();
+						}
+					}
+				} while(retry);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			setTableToDefault();
+			initEdit();
+		});
+		
+		editDeleteRow.addActionListener(e -> {
+			DatabaseMetaData meta = null;
+			if(JOptionPane.showConfirmDialog(this, "Are you sure you want to delete this row?", "Are you Sure?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+				try {
+					meta = sqlCon.getMetaData();
+					ResultSet set = meta.getPrimaryKeys(null, null, ProgramManager.getTable());
+					String primary = "", key = "";
+					while(set.next()) {
+					      primary = set.getString("COLUMN_NAME");
+					      System.out.println("getPrimaryKeys(): columnName=" + primary);
+					}
+					key = editResultSet.getString(primary);
+					String sql = String.format("DELETE FROM %s WHERE %s = '%s';", ProgramManager.getTable(), primary, key);
+					System.out.println(sql);
+					boolean retry = false;
+					do {
+						try {
+							sqlState.executeUpdate(sql);
+							JOptionPane.showMessageDialog(this, "Deleted Row", "Success", JOptionPane.INFORMATION_MESSAGE);
+						} catch (Exception e2) {
+							if(e2 instanceof CommunicationsException) {
+								connect();
+								retry = true;
+							} else {
+								JOptionPane.showMessageDialog(this, e2.getMessage(), e2.getClass().getName(), JOptionPane.ERROR_MESSAGE);
+								e2.printStackTrace();
+							}
+						}
+					} while(retry);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				setTableToDefault();
+				initEdit();
+			}
+		});
+		
 		editPanel = new JPanel();
 		editPanel.setLayout(new BorderLayout());
 		editPanel.add(editContentPanel, BorderLayout.CENTER);
 		editPanel.add(editButtons, BorderLayout.SOUTH);
 		initEdit();
-		
-		tabbedView.addTab("Browse", browseScrollPane);
-		tabbedView.addTab("Edit", editPanel);
-
-		getContentPane().add(top, BorderLayout.NORTH);
-		getContentPane().add(tabbedView, BorderLayout.CENTER);
-		
-		setSize(800, 600);
-		setLocationRelativeTo(caller);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		updateTitle();
-		setVisible(true);
 	}
-	
+
 	private void updateTitle() {
 		setTitle(ProgramManager.host + "/" + ProgramManager.database + ": " + ProgramManager.getTable() + " | Database Admin");
 	}
